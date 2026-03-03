3d1008d7a8c4c1d89ddb833f4e46d71842b0a1d4
Quah Xuanyu <xuanthegreat@gmail.com>
2026-03-03 11:58:26 +0800

    feat: finished ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,14 +1,45 @@
 #include "BitcoinExchange.hpp"
 
+static bool	isLeapYear(int year)
+{
+	return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+}
+
 bool validate_value(float value)
 {
-
+    if (value < 0)
+    {
+        throw std::runtime_error("not a positive number.");
+    }
+    if (value > 1000)
+    {
+        throw std::runtime_error("too large a number.");
+    }
+    return true;
 }
 
 bool validate_date(std::string date)
 {
+    if (date.length() != 10 || date[4] != '-' || date[7] != '-')
+        throw std::runtime_error("invalid date format => " + date);
+
+    int year = std::atoi(date.substr(0, 4).c_str());
+    int month = std::atoi(date.substr(5, 2).c_str());
+    int day = std::atoi(date.substr(8, 2).c_str());
+    if (year < 2009 || month < 1 || month > 12 || day < 1 || day > 31)
+        throw std::runtime_error("invalid date => " + date);
+
+    int daysInMonths[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (isLeapYear(year))
+        daysInMonths[1] = 29;
     
-    throw std::exception();
+    if (day > daysInMonths[month - 1]) // ensure number of days is valid for the month
+        throw std::runtime_error("invalid date => " + date);
+    
+    if (year == 2009 && month < 1 && day < 2) // ensure date is not before 2009-01-01
+        throw std::runtime_error("date before 2009-01-01 => " + date);
+    
+    return true;
 }
 
 void run_analysis(BitcoinExchange &btc, std::ifstream &inputStream)
@@ -18,22 +49,27 @@ void run_analysis(BitcoinExchange &btc, std::ifstream &inputStream)
     std::getline(inputStream, line); // Skip header
     while (std::getline(inputStream, line))
     {
-        size_t post = line.find('|');
-        std::string date = line.substr(0, post - 1);
-        float value = strtof(line.substr(post + 2).c_str(), NULL);
-
         try
         {
+            size_t post = line.find('|');
+            if (post == std::string::npos)
+            {
+                throw std::runtime_error("bad input => " + line);
+            }
 
+            std::string date = line.substr(0, post - 1);
+            validate_date(date);
+
+            float value = strtof(line.substr(post + 1).c_str(), NULL);
+            validate_value(value);
+            
+            std::cout << date << " => " << value << " = " << btc.getExchangeRate(date) * value << std::endl;
         }
         catch (const std::exception &e)
         {
-            std::cerr << "Error: " << e.what() << " => " << line << std::endl;
+            std::cerr << "Error: " << e.what() << std::endl;
             continue;
         }
-
-
-        std::cout << date << " => " << value << " = " << btc.getExchangeRate(date) * value << std::endl;
     }
 }
 
